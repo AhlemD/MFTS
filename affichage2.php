e3d6bfff0deeed64d3613f6d0c90e36ba37e52cf
AhlemD <128710694+AhlemD@users.noreply.github.com>
2023-04-07 01:49:44 +0200

    Add files via upload

---
+++ affichage2.php
@@ -0,0 +1,27 @@
+<?php
+// Établir une connexion avec la base de données
+$conn = mysqli_connect("localhost", "root", "", "datasave");
+
+// Vérifier la connexion
+if (!$conn) {
+    die("Erreur de connexion : " . mysqli_connect_error());
+}
+
+// Effectuer une requête SQL pour récupérer le nombre de données restantes dans la table
+$sql = "SELECT COUNT(*) AS total FROM stockmp WHERE emplacementparzone LIKE '%zone2%' ";
+$result = mysqli_query($conn, $sql);
+
+// Vérifier si la requête a réussi
+if (!$result) {
+    die("Erreur de requête : " . mysqli_error($conn));
+}
+
+// Récupérer le nombre de données restantes dans la table
+
+while ($row = mysqli_fetch_assoc($result)) {
+	echo '<tr>';
+	echo '<td>' . $row['total'] . '</td>';
+	echo '</tr>';
+}
+
+?>
---
+++ zone-B.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FMTS</title>
+    <link rel="stylesheet" href="../style/zone-x.css" >
+    </head>
+    
+<body>   
+    <h1>Track your material flow</h1> 
+	<div class="navbar">
+		<div class="dropdown">
+		  <button class="dropbtn">Stock matiéres premiéres</button>
+		  <div class="dropdown-content">
+			<a href="../page/zone-x.php">Zone 1</a>
+			<a href="../page/zone-2.php">Zone 2</a>
+			<a href="../page/zone-3.php">Zone 3</a>
+		  </div>
+		</div>
+		<div class="dropdown">
+			<button class="dropbtn">Production </button>
+			<div class="dropdown-content">
+            <a href="../page/zones.html">Extrusion</a>
+            <a href="#">Impression</a>
+			</div>
+		</div>
+		<div class="dropdown">
+			<button class="dropbtn">Stock produits finis</button>
+			<div class="dropdown-content">
+				<a href="../page/zone-A.php">Zone A</a>
+				<a href="../page/zone-B.php">Zone B</a>
+				<a href="../page/zone-C.php">Zone C</a>
+				</div>
+		</div> 
+	</div>
+
+	<div id="Container">
+        <p>Quantité de Stock</p>
+		
+        
+		<table>
+			<tr>
+				<th id="total" name="total" ></th>
+			</tr>
+    <?php include '../affichage/affichageB.php';?>
+        </table>
+	
+</div>
+
+<table id="mm">
+    <tr>
+        <th id="Palette" name="codepalette" >Palette</th>
+        <th id="emplacement" name="emplacement" >Emplacement</th>
+        <th id="codeabarre" name="codeabarre">code a barre</th>
+        <th id="dateheure" name="dateheure" >Date et heure dentree</th>
+    </tr>
+    <?php include '../table/tableB.php';?>
+</table>
+</body>
+
+		</html>
